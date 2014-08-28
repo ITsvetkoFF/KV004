64183f7b0dae24e5427534b41ff55a4bc7dadac8
VitaliiBorodii <vitalii@email.ua>
2014-08-28 18:32:15 +0300

    Update adminAPI.js

--- backend/adminAPI.js
+++ backend/adminAPI.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var express    = require('express');
 var app = express();
-
+var secret = require('./config/secret');
 var connectionpool = mysql.createPool({
       host     : 'localhost',
       user     : 'root',
@@ -9,11 +9,11 @@ var connectionpool = mysql.createPool({
       database : 'enviromap',
     });
 app.get('/not_aprooved', notAproovedProblems);
-app.delete('/delete_problem', deleteProblem);
-app.delete('/delete_user', deleteUser);
-app.delete('/delete_comment', deleteComment);
-app.put('/edit_problem', editProblem);
-
+app.delete('/problem', deleteProblem);
+app.delete('/user', deleteUser);
+app.delete('/comment', deleteComment);
+app.delete('/photo', deletePhoto);
+app.put('/edit', editProblem);
 function notAproovedProblems(req, res) {
 connectionpool.getConnection(function(err, connection) {
 		if (err) {
@@ -25,159 +25,281 @@ connectionpool.getConnection(function(err, connection) {
 				err:    err.code
 			});
 		} else {
-            console.log("selectNotAproovedProblems - method works");
-            var queryString='SELECT Id, Title FROM Problems WHERE Moderation IS NULL;';
-            connection.query(queryString, function(err, rows, fields) {
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
+            var token;
+            if(req.cookies.token) {
+                token = req.cookies.token;
+            } else {
+                return res.send(401);
+            }
+
+            jwt.verify(token, secret.secretToken, function(err, decoded) {
+                if(err) {
+                    return res.send(401);
                 }
-                res.send({
-                    result: 'success',
-                    err:    '',
-                    json:   rows,
-                    length: rows.length
-                });
-                connection.release();
-            });
+                if (decoded.role != 'administrator') {
+                    return res.send(401);
+                }
+                connection.query('SELECT Problems.Title, Activities.Date FROM Problems JOIN Activities ON Problems.Id = Activities.Id WHERE Moderation IS NULL;', function(err, rows, fields) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.send({
+                            result: 'error',
+                            err:    err.code
+                        });
+                    }
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                    connection.release();
+                    });
+            });    
         }
     });
 };
 function deleteProblem(req, res) {
 connectionpool.getConnection(function(err, connection) {
-		if (err) {
-			console.error('CONNECTION error: ',err);
-			res.statusCode = 503;
-			res.send({
-				result: 'error',
-				err:    err.code
-			});
-		} else {
-            console.log("getProblems - method works");
-            var id=req.body.problem_id;
-            connection.query('DELETE FROM Problems WHERE Id=' + id+";", function(err, rows, fields) {
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                res.send({
-                    result: 'success',
-                    err:    '',
-                    json:   rows,
-                    length: rows.length
-                });
-                connection.release();
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+
+                err:    err.code
             });
+        } else {
+            var token;
+            if(req.cookies.token) {
+                token = req.cookies.token;
+            } else {
+                return res.send(401);
+            }
+
+            jwt.verify(token, secret.secretToken, function(err, decoded) {
+                if(err) {
+                    return res.send(401);
+                }
+                if (decoded.role != 'administrator') {
+                    return res.send(401);
+                }
+                var id=req.body.problem_id;
+                connection.query('DELETE FROM Problems WHERE Id = ?', id, function(err, rows, fields) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.send({
+                            result: 'error',
+                            err:    err.code
+                        });
+                    }
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                    connection.release();
+                    });
+            });    
         }
     });
 };
 function deleteUser(req, res) {
 connectionpool.getConnection(function(err, connection) {
-		if (err) {
-			console.error('CONNECTION error: ',err);
-			res.statusCode = 503;
-			res.send({
-				result: 'error',
-				err:    err.code
-			});
-		} else {
-            console.log("deleteUser - method works");
-            var id=req.body.user_id;
-            connection.query('DELETE FROM Users WHERE Id=' + id+";", function(err, rows, fields) {
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                res.send({
-                    result: 'success',
-                    err:    '',
-                    json:   rows,
-                    length: rows.length
-                });
-                connection.release();
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+
+                err:    err.code
             });
+        } else {
+            var token;
+            if(req.cookies.token) {
+                token = req.cookies.token;
+            } else {
+                return res.send(401);
+            }
+
+            jwt.verify(token, secret.secretToken, function(err, decoded) {
+                if(err) {
+                    return res.send(401);
+                }
+                if (decoded.role != 'administrator') {
+                    return res.send(401);
+                }
+                var id=req.body.user_id;
+                connection.query('DELETE FROM Users WHERE Id = ?', id, function(err, rows, fields) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.send({
+                            result: 'error',
+                            err:    err.code
+                        });
+                    }
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                    connection.release();
+                    });
+            });    
         }
     });
 };
 function deleteComment(req, res) {
 connectionpool.getConnection(function(err, connection) {
-		if (err) {
-			console.error('CONNECTION error: ',err);
-			res.statusCode = 503;
-			res.send({
-				result: 'error',
-				err:    err.code
-			});
-		} else {
-            console.log("deleteComment - method works");
-            var id=req.body.comment_id;
-            connection.query('DELETE FROM Activities WHERE Id=' + id+";", function(err, rows, fields) {
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                res.send({
-                    result: 'success',
-                    err:    '',
-                    json:   rows,
-                    length: rows.length
-                });
-                connection.release();
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+
+                err:    err.code
             });
+        } else {
+            var token;
+            if(req.cookies.token) {
+                token = req.cookies.token;
+            } else {
+                return res.send(401);
+            }
+
+            jwt.verify(token, secret.secretToken, function(err, decoded) {
+                if(err) {
+                    return res.send(401);
+                }
+                if (decoded.role != 'administrator') {
+                    return res.send(401);
+                }
+                var id=req.body.comment_id;
+                connection.query('DELETE FROM Activities WHERE Id = ?', id, function(err, rows, fields) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.send({
+                            result: 'error',
+                            err:    err.code
+                        });
+                    }
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                    connection.release();
+                    });
+            });    
+        }
+    });
+};
+function deletePhoto(req, res) {
+connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+
+                err:    err.code
+            });
+        } else {
+            var token;
+            if(req.cookies.token) {
+                token = req.cookies.token;
+            } else {
+                return res.send(401);
+            }
+
+            jwt.verify(token, secret.secretToken, function(err, decoded) {
+                if(err) {
+                    return res.send(401);
+                }
+                if (decoded.role != 'administrator') {
+                    return res.send(401);
+                }
+                var id=req.body.photo_id;
+                connection.query('DELETE FROM Photos WHERE Id = ?', id, function(err, rows, fields) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.send({
+                            result: 'error',
+                            err:    err.code
+                        });
+                    }
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                    connection.release();
+                    });
+            });    
         }
     });
 };
 function editProblem(req, res) {
 connectionpool.getConnection(function(err, connection) {
-		if (err) {
-			console.error('CONNECTION error: ',err);
-			res.statusCode = 503;
-			res.send({
-				result: 'error',
-				err:    err.code
-			});
-		} else {
-            console.log("editProblem - method works");
-         var title = req.body.title,
-             content = req.body.content,
-             severity = req.body.severity,
-             moderation = req.body.moderation,
-             ProblemStatus_Id = req.body.problemStatus_Id,
-             ProblemTypes_Id = req.body.problemTypes_Id,
-             id = req.body.problem_id;
-            connection.query("UPDATE Problems SET Title="+ "\'" + title + "\'" + " , Content=" + "\'" + content + "\'" + " , Severity=" + severity+ ", Moderation="+ moderation +" , ProblemStatus_Id="+ProblemStatus_Id+", ProblemTypes_Id="+ProblemTypes_Id+ " WHERE Id="+id+";", function(err, rows, fields) {
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                res.send({
-                    result: 'success',
-                    err:    '',
-                    json:   rows,
-                    length: rows.length
-                });
-                connection.release();
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+
+                err:    err.code
             });
+        } else {
+            var token;
+            if(req.cookies.token) {
+                token = req.cookies.token;
+            } else {
+                return res.send(401);
+            }
+
+            jwt.verify(token, secret.secretToken, function(err, decoded) {
+                if(err) {
+                    return res.send(401);
+                }
+                if (decoded.role != 'administrator') {
+                    return res.send(401);
+                }
+                var data = {
+             Title : req.body.title,
+             Content : req.body.content,
+             Severity : req.body.severity,
+             Moderation : req.body.moderation,
+             ProblemStatus_Id : req.body.problemStatus_Id,
+             ProblemTypes_Id : req.body.problemTypes_Id
+                };
+            var id = req.body.problem_id;
+            connection.query("UPDATE Problems SET ? WHERE Id = ?", [data, id], function(err, rows, fields) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.send({
+                            result: 'error',
+                            err:    err.code
+                        });
+                    }
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                    connection.release();
+                    });
+            });    
         }
     });
 };
