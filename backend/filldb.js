aebbc1574be52c5b2522c356b8a7bf607f2daf5a
Stanislav Dzhus <blablapolicja@gmail.com>
2014-08-28 19:25:00 +0300

    Update filldb.js

--- backend/filldb.js
+++ backend/filldb.js
@@ -1,22 +1,28 @@
 var mysql = require('mysql'),
     crypto = require('crypto'),
-    problemStatus = ['нова', 'в процесі', 'вірішена'],
-    problemType = ['проблеми лісів', 'сміттєзвалища', 'незаконна забудова', 'проблеми водойм', 'загрози біорізноманіттю', 'браконьєрство', 'інші проблеми'],
-    title = ['problem1', 'problem2', 'problem3', 'problem4', 'problem5', 'problem6', 'problem7'],
-    content = ['content1', 'content2', 'content3', 'content4', 'content5', 'content7', 'content8'],
-    userNames = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8'],
-    userEmails = ['name1@.com', 'name2@.com', 'name3@.com', 'name4@.com', 'name5@.com', 'name6@.com', 'name7@.com', 'name8@.com'],
-    severity = [4, 3, 10, 2, 8, 9, 12],
+    secret = require('./config/secret');
+
+
+var problemTypes = ['проблеми лісів', 'сміттєзвалища', 'незаконна забудова', 'проблеми водойм', 'загрози біорізноманіттю', 'браконьєрство', 'інші проблеми'],
+    userRoles = ['administrator', 'user'],
+    
+    amountOfProblems = 50;
+    titles = ['problem1', 'problem2', 'problem3', 'problem4', 'problem5', 'problem6', 'problem7', 'problem8', 'problem9', 'problem10'],
+    content = ['content1', 'content2', 'content3', 'content4', 'content5', 'content6', 'content7', 'content8','content9','content10'],
     moderation = [true, false],
-    votes = [15, 43, 21, 1, 76, 3, 0],
-    lat = [12.65, 13.22, 1.12, 12.123, 7.16, 3.1234, 0.1],
-    problemStatusId = [1, 2, 3],
-    problemTypesId = [1, 2, 3, 4, 5, 6, 7],
-    lon = [12.65, 13.22, 1.12, 12.123, 7.16, 3.1234, 0.1],
-    activityType = ['like', 'photoPosted', 'commentPosted', 'problemAdded'],
-    userId = [1, 2, 3, 4, 5, 6, 7, 8],
-    contentStory = [],
-    passwords = [100, 110, 120, 130, 140, 150, 160, 170, 180];
+    latitude = [50.4546600, 50.282, 50.12, 50.123, 50.176, 50.1234, 50.01],
+    longtitude = [30.5238000, 50.22, 50.152, 50.1223, 50.16, 50.12034, 50.1],
+    
+    userNames = ['admin', 'name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8', 'name9'],
+    userEmails = ['admin@.com', 'name1@.com', 'name2@.com', 'name3@.com', 'name4@.com', 'name5@.com', 'name6@.com', 'name7@.com', 'name8@.com', 'name9@.com'],
+    passwords = ['admin', '12345678'],
+    
+    
+    activityTypes = ['addProblem', 'editProblem', 'voteForProblem', 'postPhoto', 'postComment'];
+
+var i;
+   
+   
 
 function randomIntInc(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
@@ -27,76 +33,83 @@ var connection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'root',
-        database: 'enviromap'
+        password: '',
+        database: 'Enviromap'
     });
 
 
-function fillProblemStatus() {
-    for (var i = 0; i < problemStatus.length; i++) {
-        connection.query('INSERT INTO ProblemStatus SET ?', {probStatus: problemStatus[i]});
-    }
-}
-
 
 function fillProblemTypes() {
-    for (var i = 0; i < problemType.length; i++) {
-        connection.query('INSERT INTO ProblemTypes SET ?', {probType: problemType[i]});
+    for (i = 0; i < problemTypes.length; i++) {
+        connection.query('INSERT INTO ProblemTypes SET ?', {type: problemTypes[i]});
     }
 }
 
-
-function fillProblemAndActivityTable() {
-    for (var i = 0; i < 50; i++) {
-        var currentContent = content[randomIntInc(0, content.length)];
-        contentStory.push(currentContent);
-        connection.query('INSERT INTO Problems SET ?', {title: title[randomIntInc(0, title.length)],
-            content: currentContent,
-            severity: severity[randomIntInc(0, severity.length)],
-            moderation: moderation[randomIntInc(0, moderation.length)],
-            votes: votes[randomIntInc(0, votes.length)],
-            lat: lat[randomIntInc(0, lat.length)],
-            lon: lon[randomIntInc(0, lon.length)],
-            problemStatus_Id: problemStatusId[randomIntInc(0, problemStatusId.length)],
-            problemTypes_Id: problemTypesId[randomIntInc(0, problemTypesId.length)]
-        });
-    }
-
-    for (var j = 1; j <= 50; j++) {
-        connection.query('INSERT INTO Activities SET ?', {activityContent: contentStory[j - 1],
-            problems_id: j,
-            activityDate: new Date(),
-            activityTypes_Id: 4,
-            users_id: userId[randomIntInc(0, userId.length)]
+function fillUserRoles() {
+    for (i = 0; i < userRoles.length; i++) {
+        connection.query('INSERT INTO UserRoles SET ?', {
+            id: i+1,
+            role: userRoles[i]
         });
     }
 }
-
 
 function fillUsers() {
-    for (var i = 0; i < userNames.length; i++) {
+    connection.query('INSERT INTO Users SET ?', {
+            name: userNames[0],
+            password: crypto.createHmac('sha1', secret.secretToken).update(passwords[0]).digest("hex"),
+            email: userEmails[0],
+            userRoles_Id: 1});
+    for (i = 1; i < userNames.length; i++) {
         connection.query('INSERT INTO Users SET ?', {
             name: userNames[i],
-            password: crypto.createHmac('sha1', '33').update(new Buffer(passwords[i])).digest("hex"),
-            email: userEmails[i]});
+            password: crypto.createHmac('sha1', '123456').update(passwords[1]).digest("hex"),
+            email: userEmails[i],
+            userRoles_Id: 2});
+    }
+}
+    
+  
+
+
+function fillActivityTypes() {
+    for (var i = 0; i < activityTypes.length; i++) {
+        connection.query('INSERT INTO ActivityTypes SET ?', {name: activityTypes[i]});
     }
 }
 
-
-function fillActivityType() {
-    for (var i = 0; i < activityType.length; i++) {
-        connection.query('INSERT INTO ActivityTypes SET ?', {activityName: activityType[i]});
+function fillProblemsActivities() {
+    for (i = 0; i < amountOfProblems; i++) {
+        connection.query('INSERT INTO Problems SET ?', {
+            title: titles[randomIntInc(0, titles.length)],
+            content: content[randomIntInc(0, content.length)],
+            severity: randomIntInc(1, 5),
+            moderation: moderation[randomIntInc(0, moderation.length)],
+            votes: randomIntInc(0, 100),
+            latitude: latitude[randomIntInc(0, latitude.length)],
+            longtitude: longtitude[randomIntInc(0, longtitude.length)],
+            status: moderation[randomIntInc(0, moderation.length)],
+            problemTypes_Id: randomIntInc(1, problemTypes.length)
+        });
     }
+    
+    for (i = 0; i < amountOfProblems; i++) {
+        connection.query('INSERT INTO Activities SET ?', {
+            date: new Date(),
+            activityTypes_Id: 1,
+            users_Id: randomIntInc(1, userNames.length),
+            problems_Id: i+1
+            
+        });
+    }   
 }
 
-function filldb() {
-    fillProblemStatus();
-    fillProblemTypes();
-    fillUsers();
-    fillActivityType();
-    fillProblemAndActivityTable();
-}
+fillProblemTypes(); 
+fillUserRoles();
+fillUsers();
+fillActivityTypes();
+fillProblemsActivities();
 
-filldb();
+
 console.log('database filled');
 connection.end();
