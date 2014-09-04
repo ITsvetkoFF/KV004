0ccc995e49da8fe0387284095d43051ba44a65ce
VitaliiBorodii <vitalii@email.ua>
2014-09-04 17:32:44 +0300

    Update FillMySQLTable.js

--- FillMySQLTable.js
+++ FillMySQLTable.js
@@ -12,28 +12,23 @@ var mysql = require('mysql');
 var connection = mysql.createConnection(
     {
       host     : 'localhost',
-      user     : 'root',
-      password : 'root',
-      database : 'enviromap',
+    user     : 'root',
+    password : 'root',
+    database : 'Enviromap'
     }
 );
 var problemStatus = ['Нова', 'Вирішена'],
 problemType = ['Проблеми лісів', 'Сміттєзвалища', 'Незаконна забудова', 'Проблеми водойм', 'Загрози біорізноманіттю', 'Браконьєрство', 'Інші проблеми'];
 connection.connect();
-for (var i = 0; i < problemStatus.length; i++) {
-        connection.query('INSERT INTO ProblemStatus SET ?', {probStatus: problemStatus[i]}, function(err, rows, fields) {
-    if (err) throw err;
-    });
-};
 for (var i = 0; i < problemType.length; i++) {
-        connection.query('INSERT INTO ProblemTypes SET ?', {probType: problemType[i]}, function(err, rows, fields) {
+        connection.query('INSERT INTO ProblemTypes SET ?', {Type: problemType[i]}, function(err, rows, fields) {
     if (err) throw err;
     });
 };
 
 for (var i=0, len=probs.length; i<len; i++) {
-if (probs[i].probStatus == 'Нова') probs[i].probStatus = 1;
-else if (probs[i].probStatus == 'Вирішена') probs[i].probStatus = 2;
+if (probs[i].probStatus == 'Нова') probs[i].probStatus = 0;
+else if (probs[i].probStatus == 'Вирішена') probs[i].probStatus = 1;
 
 if (probs[i].probType == 'Проблеми лісів') probs[i].probType = 1;
 else if (probs[i].probType == 'Сміттєзвалища') probs[i].probType = 2;
@@ -48,15 +43,17 @@ probs[i].content = probs[i].content.replace(/'/g,"\\'");
 probs[i].title = probs[i].title.replace(/'/g,"\\'");
 };
 
-if ( (probs[i].probType) && (probs[i].probStatus) ) {
+if (!probs[i].lat || !probs[i].lo) {
+if (!probs[i].probType) probs[i].probType=7;
 var data = {
 Title : probs[i].title,
 Content : probs[i].content,
 Severity : probs[i].severity,
-Moderation : probs[i].moderation,
-Lat : probs[i].lat,
-Lon : probs[i].lon,
-ProblemStatus_Id : probs[i].probStatus,
+Moderation : 0,
+Votes : probs[i].votes,
+Latitude : probs[i].lat,
+Longtitude : probs[i].lon,
+Status : probs[i].probStatus,
 ProblemTypes_Id : probs[i].probType
   };
 connection.query("INSERT INTO Problems SET ?", data, function(err, rows, fields) {
