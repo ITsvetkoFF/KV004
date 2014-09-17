993ccbef7f433c2d0f04b7b79eecf9c347bf61d2
VitaliiBorodii <vitalii@email.ua>
2014-09-17 11:38:56 +0300

    Update resources.js

--- frontend/app/controllers/resources.js
+++ frontend/app/controllers/resources.js
@@ -2,15 +2,8 @@ define(['./module'], function (controllers) {
 	'use strict';
     controllers.controller('Resources', [ '$scope', '$http', function($scope,$http){
         
-        $http({ method: 'GET', url: 'http://localhost:8090/api/getTitles' }).success(function (data) {
-        $scope.data = data.json;
-
-        $scope.resource =function(){
-            alert();
-        }
-
-
- 
+        $http({ method: 'GET', url: 'http://ita-kv.tk:8090/api/getTitles' }).success(function (data) {
+        $scope.data = data;
  });
     }]);
 });
