418d8d9dd5cce147f00f4d21d545b56ac33eb14d
VitaliiBorodii <vborodtc@KVCLASS08.training.local>
2014-09-19 17:23:34 +0300

    make improoves

--- frontend/app/controllers/editorCtrl.js
+++ frontend/app/controllers/editorCtrl.js
@@ -4,16 +4,23 @@ define(['./module'], function (controllers) {
 		if ($routeParams.Alias) {
 		$http.get('http://ita-kv.tk:8090/api/resources/' + $routeParams.Alias).success(function(data) {
             $scope.resource = data[0];
-            });
-		}
-		console.log($scope.resource);
-		if ($scope.resource) {
-			$scope.Alias = $routeParams.Alias;
+            $scope.Alias = $routeParams.Alias;
 			$scope.Content = $scope.resource.Content;
 			$scope.Title = $scope.resource.Title;
+            });
 		}
 		$rootScope.$broadcast('Update', '_full');
-	}])
-});
-
-
+		$scope.sendResource = function(Alias, Content, Title) {
+			if (Alias == '' || Content == '' || Title == '') {
+                $scope.delPhotoErrorMsg = 'Всі поля обов\'язкові для заповнення';
+                return;
+            }
+            $http.post('http://ita-kv.tk:8090' + '/api/editResource', {Alias: Alias, Content: Content, Title: Title}).success(function() {
+            console.log('Query has been successfully sent');
+           }).error(function(status, data) {
+                    console.log(status);
+                    console.log(data);
+                });
+		}
+	}]);
+		});
