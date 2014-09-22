89b7c2eabcd2623eb7965168395ef0bcca48ded6
VitaliiBorodii <vitalii@email.ua>
2014-09-22 12:29:22 +0300

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -78,6 +78,9 @@ app.delete('/api/user/:id', routes.deleteUser);
 app.delete('/api/activity/:id', routes.deleteComment);
 app.delete('/api/photo/:id', routes.deletePhoto);
 app.put('/api/edit', routes.editProblem);
+app.post('/api/addResource', routes.addResource);
+app.put('/api/editResource/:alias', routes.editResource);
+app.delete('/api/deleteResource/:alias', routes.deleteResource);
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
