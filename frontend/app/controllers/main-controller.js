5f4c4ca9cb44ac28567a4d01a9572278beaa7db8
andrey-abramow <abramow.andrey@gmail.com>
2014-09-24 10:30:22 +0300

    Merge branch 'master' of https://github.com/ITsvetkoFF/KV004
    Conflicts:
    	frontend/app/controllers/main-controller.js
    	frontend/app/controllers/resources.js

--- frontend/app/controllers/main-controller.js
+++ frontend/app/controllers/main-controller.js
@@ -1,17 +1,27 @@
 define(['./module'],function(controllers){
     'use strict';
     controllers.controller('mainCtrl',['$scope','$rootScope', '$http' ,function($scope,$rootScope, $http){
+        $scope.getTitles = function() {
+            $http({ method: 'GET', url: 'http://ita-kv.tk:8090/api/getTitles' }).success(function (data) {
+                $scope.data = data;
+                for (var i = 0; i < $scope.data.length; i++) {
+                    if ($scope.data[i].Alias === 'about') $scope.data.splice(i,1);
+                }
+            });
+            $scope.deleteResource = function(Alias) {
+                $http.delete('/api/deleteResource/' + Alias)
+            };
+        };
 
         $scope.showRigthSide = "_hide";
 
-
         $scope.hideDiv = function() {
-            $rootScope.$broadcast('Update', '_hide');
+            $scope.showRigthSide = "_hide";
         };
         $rootScope.$on("Update", function(event, message) {
             if ($scope.showRigthSide != message){
-            $scope.showRigthSide = message;
-        }
+                $scope.showRigthSide = message;
+            }
         });
     }]);
 });
