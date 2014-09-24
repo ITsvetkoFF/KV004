44e19fe47d744284bd319c6970a0bb1f879ea2e4
VitaliiBorodii <vitalii@email.ua>
2014-09-24 18:29:38 +0300

    Update main-controller.js

--- frontend/app/controllers/main-controller.js
+++ frontend/app/controllers/main-controller.js
@@ -2,7 +2,17 @@ define(['./module'],function(controllers){
     'use strict';
     controllers.controller('mainCtrl',['$scope','$rootScope', '$http' ,function($scope,$rootScope, $http){
         $scope.showRigthSide = "_hide";
-
+        $scope.getTitles = function() {
+            $http({ method: 'GET', url: 'http://ita-kv.tk:8090/api/getTitles' }).success(function (data) {
+                $scope.data = data;
+                for (var i = 0; i < $scope.data.length; i++) {
+                    if ($scope.data[i].Alias === 'about') $scope.data.splice(i,1);
+                }
+            });
+            $scope.deleteResource = function(Alias) {
+                $http.delete('/api/deleteResource/' + Alias)
+            };
+        };
         $scope.hideDiv = function() {
             $scope.showRigthSide = "_hide";
         };
