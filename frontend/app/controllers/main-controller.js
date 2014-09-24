7709d0d99fc954922b5af4fe039710b161dabce7
andrey-abramow <abramow.andrey@gmail.com>
2014-09-24 10:38:51 +0300

    Update main-controller.js

--- frontend/app/controllers/main-controller.js
+++ frontend/app/controllers/main-controller.js
@@ -2,7 +2,7 @@ define(['./module'],function(controllers){
     'use strict';
     controllers.controller('mainCtrl',['$scope','$rootScope', '$http' ,function($scope,$rootScope, $http){
         $scope.getTitles = function() {
-            $http({ method: 'GET', url: 'http://ita-kv.tk:8090/api/getTitles' }).success(function (data) {
+            $http({ method: 'GET', url: 'api/getTitles' }).success(function (data) {
                 $scope.data = data;
                 for (var i = 0; i < $scope.data.length; i++) {
                     if ($scope.data[i].Alias === 'about') $scope.data.splice(i,1);
