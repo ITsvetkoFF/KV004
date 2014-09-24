c8b1d6e00a5b5046fa26a9d3cfe7cb658ac7259a
VitaliiBorodii <vitalii@email.ua>
2014-09-24 17:15:31 +0300

    Update main-controller.js

--- frontend/app/controllers/main-controller.js
+++ frontend/app/controllers/main-controller.js
@@ -1,18 +1,6 @@
 define(['./module'],function(controllers){
     'use strict';
     controllers.controller('mainCtrl',['$scope','$rootScope', '$http' ,function($scope,$rootScope, $http){
-        $scope.getTitles = function() {
-            $http({ method: 'GET', url: 'api/getTitles' }).success(function (data) {
-                $scope.data = data;
-                for (var i = 0; i < $scope.data.length; i++) {
-                    if ($scope.data[i].Alias === 'about') $scope.data.splice(i,1);
-                }
-            });
-            $scope.deleteResource = function(Alias) {
-                $http.delete('/api/deleteResource/' + Alias)
-            };
-        };
-
         $scope.showRigthSide = "_hide";
 
         $scope.hideDiv = function() {
