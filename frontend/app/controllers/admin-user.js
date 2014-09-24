1a12e45085b3916c5fe9b86a7e31d36b3927b776
VitaliiBorodii <vitalii@email.ua>
2014-09-24 17:14:43 +0300

    Update admin-user.js

--- frontend/app/controllers/admin-user.js
+++ frontend/app/controllers/admin-user.js
@@ -8,7 +8,17 @@ define(['./module'], function (controllers) {
         $scope.name = ipCookie('userName');
         $scope.surname = ipCookie('userSurname');
         $scope.userId = ipCookie('id');
-        
+        $scope.getTitles = function() {
+            $http({ method: 'GET', url: 'http://ita-kv.tk:8090/api/getTitles' }).success(function (data) {
+                $scope.data = data;
+                for (var i = 0; i < $scope.data.length; i++) {
+                    if ($scope.data[i].Alias === 'about') $scope.data.splice(i,1);
+                }
+            });
+            $scope.deleteResource = function(Alias) {
+                $http.delete('/api/deleteResource/' + Alias)
+            };
+        };
         /*****--- Register form ---*****/
         $scope.registerClassAppendix = "_hide";
 
