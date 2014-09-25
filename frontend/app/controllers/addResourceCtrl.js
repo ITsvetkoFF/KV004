c3dd4c38067066bf6289ccd18519d3f73f6fff06
VitaliiBorodii <vitalii@email.ua>
2014-09-25 19:21:00 +0300

    Update addResourceCtrl.js

--- frontend/app/controllers/addResourceCtrl.js
+++ frontend/app/controllers/addResourceCtrl.js
@@ -2,12 +2,12 @@ define(['./module'], function (controllers) {
    'use strict';
     controllers.controller('addResourceCtrl',['$scope', '$rootScope', '$http', '$routeParams', function ($scope, $rootScope, $http, $routeParams) {
 		$rootScope.$broadcast('Update', '_full');
-		$scope.sendResource = function(Alias, Content, Title) {
-			if (Alias == '' || Content == '' || Title == '') {
+		$scope.sendResource = function(Alias, Content, Title, IsResource) {
+			if (Alias == '' || Content == '' || Title == '' || IsResource == '') {
                 $scope.delPhotoErrorMsg = 'Всі поля обов\'язкові для заповнення';
                 return;
             }
-            $http.post('/api/addResource', { Alias: Alias, Content: Content, Title: Title}).success(function() {
+            $http.post('api/addResource', { Alias: Alias, Content: Content, Title: Title, IsResource: IsResource}).success(function() {
             console.log('Query has been successfully sent');
            }).error(function(status, data) {
                     console.log(status);
