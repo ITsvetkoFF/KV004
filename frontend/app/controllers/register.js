244a0731995c0269769ae73a06a6a2b16b0e0761
Tarzak <jobtarzak@gmail.com>
2014-10-08 13:27:25 +0300

    add reg form validation

--- frontend/app/controllers/register.js
+++ frontend/app/controllers/register.js
@@ -1,33 +1,33 @@
-/**
- * Created by taras on 01.10.14.
- */
 define(['./module'],function(controllers){
     'use strict';
     controllers.controller('registerCtrl', ['$scope','$rootScope','$modal', '$log','$http','$modalInstance' ,function ($scope,$rootScope,  $modal, $log,$http,$modalInstance){
-        console.log('registerCtrl is loaded');
-        $scope.registerNewUser = function(){
+
+        $scope.submitForm = function(isValid) {
+
             var data = {};
             data.first_name = document.registerForm.first_name.value;
             data.last_name = document.registerForm.last_name.value;
             data.email = document.registerForm.email.value;
             data.password = document.registerForm.password.value;
 
-            $http.post('api/register', data);
-            
-            var mainModal = document.querySelector('.modal-content');
-            var modal1 = document.querySelector('.modal-header');
-            var modal2 = document.querySelector('.modal-body');
-            mainModal.removeChild(modal1);
-            mainModal.removeChild(modal2);
+            // check to make sure the form is completely valid
+            if (isValid  && ($scope.user.password == $scope.user.password_second)) {
+
+                 $http.post('api/register', data).success(function(data, status, headers, config) {
+
+                     $scope.formHide = true;
+
+                     $scope.alerts.push({type: 'success', msg: 'Ви успішно зареєструвались!'});
+                 }).error(function(data, status, headers, config) {
+                    if (status == 400)
+                        $scope.wrongEmail = "Ваша пошта вже зайнята!";
+                 });
+            }
         };
 
         $scope.alerts = [];
 
-        $scope.addAlert = function() {
-            console.log($scope.alerts);
-            $scope.alerts.push({type: 'success', msg: 'Ви успішно зареєструвались!'});
-        };
-        $scope.closeAlert = function(index) {
+        $scope.closeAlert = function() {
             $modalInstance.close(console.log('alert closed'));
         };
 
