ff7a5f4b38568db5f76c2fb59742d7194d63f4af
andrey-abramow <abramow.andrey@gmail.com>
2014-10-23 18:38:56 +0300

    Update activity-directive.js

--- frontend/app/directives/activity-directive.js
+++ frontend/app/directives/activity-directive.js
@@ -2,7 +2,6 @@ define(['./module'],function(directives){
     directives.directive('activity',function(){
         return {
             restrict: 'EA',
-            controller: 'showProblemCtrl',
             templateUrl: 'app/templates/activity.html'
 
         };
